d7e5ced09dbac5b5ece0067f60ebc283f99cee53
Doğa Can Kılıç <dogabeey@gmail.com>
2025-07-17 18:32:24 +0300

    container movement code

--- Assets/Scripts/Game/ContainerManager.cs
+++ Assets/Scripts/Game/ContainerManager.cs
@@ -62,8 +62,17 @@ public class ContainerManager : MonoBehaviour
         }
     }
 
+    // Send the first container away to the designated move point and move the next container to the first position, then the second to the second position, and so on.
     private void SendNextContainer()
     {
+        Container sentAwayContainer = containers[0];
+        sentAwayContainer.transform.DOMove(containerMovePoint.position, containerMoveDuration).SetEase(movementEase);
+
         containers.RemoveAt(0);
+
+        foreach(Container container in containers)
+        {
+            container.transform.DOMove(container.transform.localPosition + containerOffset, containerMoveDuration).SetEase(movementEase);
+        }
     }
 }
